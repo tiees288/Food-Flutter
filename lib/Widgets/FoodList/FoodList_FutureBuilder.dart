8e6a71e258b20e4c6f6b4560540dce821b17f605
“KrittawatMC” <tiees288@gmail.com>
2023-01-14 21:21:22 +0700

    Future Builder

--- lib/Widgets/FoodList/FoodList_FutureBuilder.dart
+++ lib/Widgets/FoodList/FoodList_FutureBuilder.dart
@@ -3,14 +3,14 @@ import '../../Models/Food.dart';
 import '../list_containers.dart';
 import 'package:helloapp/ApiHelpers/Food.dart';
 
-class listFood extends StatefulWidget {
-  const listFood({super.key});
+class foodListFututre extends StatefulWidget {
+  const foodListFututre({super.key});
 
   @override
-  State<listFood> createState() => _listFoodState();
+  State<foodListFututre> createState() => _foodListFututreState();
 }
 
-class _listFoodState extends State<listFood> {
+class _foodListFututreState extends State<foodListFututre> {
   List<Food> FoodList = List.empty();
   bool isLoading = true;
 
@@ -22,56 +22,59 @@ class _listFoodState extends State<listFood> {
 
   Future<List<Food>> fetchFood() async {
     List<Food> responseFood = await FetchFood();
-    setState(() {
-      FoodList = responseFood;
-      isLoading = false;
-    });
-    // print(FoodList);
-    return FoodList;
+    return responseFood;
   }
 
   @override
   Widget build(BuildContext context) {
-    return (isLoading == true
-        ? LinearProgressIndicator(
-            backgroundColor: Colors.grey.shade50,
-            valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
-          )
-        : ListView.builder(
-            itemCount: FoodList.length,
-            itemBuilder: (BuildContext context, int index) {
-              String foodName = FoodList[index].name;
-              String foodImg = FoodList[index].imgPath;
-              String foodDesc = FoodList[index].description;
-              return MainContainer(
-                child: Row(
-                  children: [
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height,
-                      width: (MediaQuery.of(context).size.width / 2) - 20,
-                      child: Image.asset(foodImg),
+    return FutureBuilder(
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            var data = snapshot.data;
+            return ListView.builder(
+                itemCount: data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  String foodName = data[index].name;
+                  String foodImg = data[index].imgPath;
+                  String foodDesc = data[index].description;
+                  return MainContainer(
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height,
+                          width: (MediaQuery.of(context).size.width / 2) - 20,
+                          child: Image.asset(foodImg),
+                        ),
+                        Container(
+                            alignment: Alignment.topCenter,
+                            padding: EdgeInsets.only(left: 10),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '$foodName',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w300),
+                                ),
+                                Text('$foodDesc',
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                    )),
+                              ],
+                            ))
+                      ],
                     ),
-                    Container(
-                        alignment: Alignment.topCenter,
-                        padding: EdgeInsets.only(left: 10),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '$foodName',
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.w300),
-                            ),
-                            Text('$foodDesc',
-                                style: TextStyle(
-                                  fontSize: 14,
-                                )),
-                          ],
-                        ))
-                  ],
-                ),
-              );
-            }));
+                  );
+                });
+          }
+          return LinearProgressIndicator(
+            minHeight: 1,
+            backgroundColor: Colors.grey[200],
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+          );
+        },
+        future: fetchFood());
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+// Libs
 import 'package:flutter/services.dart';
 import 'package:helloapp/ApiHelpers/Food.dart';
+import 'package:flutter/cupertino.dart';
+// Widgets
 import 'Widgets/Carts.dart';
 import 'Widgets/button.dart';
-import 'Widgets/FoodList.dart';
+import 'Widgets/FoodList/FoodList_ListBuilder.dart';
+import 'Widgets/FoodList/FoodList_FutureBuilder.dart';
 import 'Models/Food.dart';
 import 'package:flutter/material.dart';
 import 'Widgets/list_containers.dart';
-import 'package:flutter/cupertino.dart';
+import 'Widgets/BottomApp.dart';
 
 main() {
   runApp(const MyAppWid());
@@ -32,68 +36,9 @@ class MyAppWid extends StatelessWidget {
             ),
             backgroundColor: Colors.grey.shade50,
           ),
-          bottomNavigationBar: BottomAppBar(
-            elevation: 1.0,
-            color: Colors.grey.shade50,
-            child: Container(
-              height: 40,
-              child: Padding(
-                padding: EdgeInsets.only(top: 6, bottom: 12),
-                child: Wrap(
-                  alignment: WrapAlignment.center,
-                  spacing: 50,
-                  children: [
-                    InkWell(
-                        onTap: () {
-                          print("HOme");
-                        },
-                        child: Container(
-                            width: 50,
-                            // heigh/t: 4,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            child: Column(children: [
-                              Icon(
-                                Icons.home,
-                                color: Colors.black,
-                              ),
-                              Text(
-                                "Home",
-                                style: TextStyle(fontSize: 12),
-                              )
-                            ]))),
-                    Wrap(children: [
-                      Column(children: [
-                        Icon(
-                          Icons.history,
-                          color: Colors.black,
-                        ),
-                        Text(
-                          "Orders",
-                          style: TextStyle(fontSize: 12),
-                        )
-                      ])
-                    ]),
-                    Wrap(children: [
-                      Column(children: [
-                        Icon(
-                          Icons.account_box,
-                          color: Colors.black,
-                        ),
-                        Text(
-                          "Accounts",
-                          style: TextStyle(fontSize: 12),
-                        )
-                      ])
-                    ]),
-                  ],
-                ),
-              ),
-            ),
-          ),
+          bottomNavigationBar: BottomApp(),
           body: Stack(children: [
-            listFood(),
+            foodListFututre(),
             FoodCart(),
           ])),
       // theme: ThemeData(primarySwatch: Colors.red.shade500),
